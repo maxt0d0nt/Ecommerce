c5935efa848559149bf106720d9c0258f46d04d3
Max Ascanio <maxbox21@hotmail.com>
2024-10-23 20:16:44 +0200

    navbar 2

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,29 +1,35 @@
 import Link from "next/link";
 import React from "react";
 import Menu from "./Menu";
-import Image from "next/image"
+import Image from "next/image";
+import SearchBar from "./SearchBar";
+import NavbarIcons from "./NavbarIcons";
 
 const Navbar = () => {
   return (
     <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
       {/* MOBILE */}
-      <div className="h-full flex items-center justify-between">
+      <div className="h-full flex items-center justify-between md:hidden">
         <Link href="/">
           <div className="text-2xl tracking-wide">LAMA</div>
         </Link>
         <Menu />
       </div>
       {/*bigger screens*/}
-      <div className="hidden md:flex items-center justify-between gap-8 h-full"></div>
-      {/*LEFT*/}
-      <div className="w-1/3">
-      <Link href="/">
-      <Image src="/logo.png" alt="" width={24} height={24}/>
-           <div className="text-2xl tracking-wide">LAMA</div>
-        </Link>
+      <div className="hidden md:flex items-center justify-between gap-8 h-full">
+        {/*LEFT*/}
+        <div className="w-1/3">
+          <Link href="/" className="flex items-center gap-3">
+            <Image src="/logo.png" alt="" width={24} height={24} />
+            <div className="text-2xl tracking-wide">LAMA</div>
+          </Link>
+        </div>
+        {/*RIGTH*/}
+        <div className="w-2/3 flex items-center justify-between gap-8">
+          <SearchBar />
+          <NavbarIcons />
+        </div>
       </div>
-      {/*RIGTH*/}
-      <div className="w-2/3"></div>
     </div>
   );
 };
---
+++ src/app/components/NavbarIcons.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react';
+
+const NavbarIcons = () => {
+    return (
+        <div>
+            NavbarIcons
+        </div>
+    );
+};
+
+export default NavbarIcons;
